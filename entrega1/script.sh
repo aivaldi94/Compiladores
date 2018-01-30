ce49f5b086c12a7b5da3c35b7b4d472e4b39b5df
aivaldi94 <aivaldi94@users.noreply.github.com>
2018-01-30 16:02:22 -0300

    s

--- entrega1/script.sh
+++ entrega1/script.sh
@@ -2,7 +2,7 @@
 
 echo -e "\n GOOD \n" > testGood.txt
 
-for (( i=1; i<=60; i++))
+for (( i=1; i<=23; i++))
 	do
 		echo -e "\n TEST"$i >> testGood.txt
 		./tiger ../tests/good/test$i.tig >> testGood.txt
@@ -10,7 +10,7 @@ for (( i=1; i<=60; i++))
 
 echo -e "\n TYPE \n" > testType.txt
 
-for (( i=1; i<=60; i++))
+for (( i=1; i<=34; i++))
 	do
 		echo -e "\n TEST"$i >> testType.txt
 		./tiger ../tests/type/test$i.tig >> testType.txt
