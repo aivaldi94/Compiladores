729c63e6441569b3f23af454dee75538bd6b8301
aivaldi94 <aivaldi94@users.noreply.github.com>
2018-01-30 15:45:15 -0300

    script

--- entrega1/script.sh
+++ entrega1/script.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-echo -e "EMPIEZA GOOD \n" >> arch.txt
+echo "EMPIEZA" > arch.txt
+
+echo "Good" >> arch.txt
 
 for (( i=1; i<=60; i++))
 	do
-		echo -e "\n TEST"$i "\n" >> arch.txt
+		echo "test"$i >> arch.txt
 		./tiger ../tests/good/test$i.tig >> arch.txt
 	done
 
-echo -e "EMPIEZA TYPE \n" >> arch.txt
+echo "Type" >> arch.txt
 
 for (( i=1; i<=60; i++))
 	do
-		echo -e "\n TEST"$i "\n" >> arch.txt
+		echo "test"$i >> arch.txt
 		./tiger ../tests/type/test$i.tig >> arch.txt
 	done
+
+ #!/bin/bash
+
+#echo -e "EMPIEZA GOOD \n" >> arch.txt
+
+#for (( i=1; i<=60; i++))
+#	do
+#		echo -e "\n TEST"$i "\n" >> arch.txt
+#		./tiger ../tests/good/test$i.tig >> arch.txt
+#	done
+
+#echo -e "EMPIEZA TYPE \n" >> arch.txt
+
+#for (( i=1; i<=60; i++))
+#	do
+#		echo -e "\n TEST"$i "\n" >> arch.txt
+#		./tiger ../tests/type/test$i.tig >> arch.txt
+#	done
 
